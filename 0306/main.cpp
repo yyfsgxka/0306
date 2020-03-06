95682e1520a153b57b8e38369fe5a454dfd3a43c
YangYF <YangYF@DESKTOP-R1NKRMF>
2020-03-06 15:09:24 +0800

    添加练习四

--- 0306/main.cpp
+++ 0306/main.cpp
@@ -22,33 +22,8 @@ int main()
 
 	waitKey(0);
 
-	//练习二
-	VideoCapture cap;//实例化一个videocapture类，名称为cap
-	cap.open(0);//
-	if (!cap.isOpened())
-	{
-		std::cout << "不可打开视频文件" << std::endl;
-		return -1;
-	}
-	double fps = cap.get(CAP_PROP_FPS);
-	std::cout << "fps" << fps << std::endl;
-	while (1)
-	{
-		cv::Mat frame;
-		bool rSucess = cap.read(frame);
-		if (!rSucess)
-		{
-			std::cout << "不能从视频中读取帧" << std::endl;
-			break;
-		}
-		else
-		{
-			cv::imshow("frame ", frame);
-		}
-		waitKey(30);
-
-	}
-		//练习三
+	
+	//练习三
 	cv::Mat dstMat = imread("C:\\Users\\YangYF\\Desktop\\0.png");
 
 	cv::Point pt;
@@ -68,7 +43,63 @@ int main()
 	rectangle(dstMat, rect, CV_RGB(255, 0, 0), 1, 8, 0);
 	imshow("dstMat", dstMat);
 	waitKey(0);
-	return 0;
+	
+
+	//练习四
+	    int histgram1[256] = { 0 };
+		float  histgram[256] = { 0 };
+		cv::Mat scrMat= imread("C:\\Users\\YangYF\\Desktop\\1.png",0);	
+		int width = scrMat.cols;
+		int height = scrMat.rows;
+		int sum = width * height;
+   
+		for (int i=0;i<256;i++)
+		{
+			histgram[i] = i;
+		}
+		for (int j = 0; j<height; j++)
+		{
+			for (int i= 0;i < width; i++)
+			{
+				int a = scrMat.at<uchar>(j, i);
+				histgram[a] = histgram[a] + 1;
+			}
+		}
+		for (int i = 0; i < 256; i++)
+		{
+			histgram[i] = histgram[i] / sum;
+			cout << histgram[i] << endl;
+
+		}
+		waitKey(0);
+		system("pause");
+		//练习二
+		VideoCapture cap;//实例化一个videocapture类，名称为cap
+		cap.open(0);//
+		if (!cap.isOpened())
+		{
+			std::cout << "不可打开视频文件" << std::endl;
+			return -1;
+		}
+		double fps = cap.get(CAP_PROP_FPS);
+		std::cout << "fps" << fps << std::endl;
+		while (1)
+		{
+			cv::Mat frame;
+			bool rSucess = cap.read(frame);
+			if (!rSucess)
+			{
+				std::cout << "不能从视频中读取帧" << std::endl;
+				break;
+			}
+			else
+			{
+				cv::imshow("frame ", frame);
+			}
+			waitKey(30);
+
+		}
+		return 0;
 }
 
 
